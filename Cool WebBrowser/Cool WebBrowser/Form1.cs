d5307f2c37f0afb3e73734f0bf1426761b8b17fb
Alumno <Alumno@PC02>
2019-09-02 10:16:46 -0500

    pagina

---
+++ Cool WebBrowser/Cool WebBrowser/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cool_WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.google.com");
+            textBox1.Text = ("www.google.com");
+        }
+    }
+}
